696c4894037e1b3b13da18945d6ab82d56d6e272
sabamansoori <sabamansoori@10.0.0.102>
2023-04-10 06:39:51 -0400

    initial commit

---
+++ src/day2hwp2/Day2hwp2Demo.java
@@ -0,0 +1,18 @@
+package day2hwp2;
+
+public class Day2hwp2Demo {
+
+	
+public static void main (String args []) {
+		
+		System.out.println ("Day 2 hw 2 stash");
+		
+	}
+	
+public void sleep () {
+System.out.println ("sleeping");
+}
+
+	
+	
+}
